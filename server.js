f9ad2fdbd1befc31f57ffc59fd1bfcb8f2255dcb
TyTinnish <ty.tinnish@gmail.com>
2026-01-10 15:33:45 -0500

    fixing local host issues

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:5173'], // Add all your dev ports
     credentials: true
 }));
 
